dffa43155698051bb516b306cc7a0c0222e48c44
unknown <jorammanoah1@gmail.com>
2021-05-03 09:45:18 -0700

    Modified navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import {Menu} from 'antd'
 
+const {SubMenu} = Menu
 export default function Navigation() {
     return (
         <Menu mode="horizontal">
             <Menu.Item>
                 Home
             </Menu.Item>
-            <Menu.Item>
-                Services
-            </Menu.Item>
-            <Menu.Item>
-                Login
-            </Menu.Item>
+            <SubMenu title="Services">
+                <Menu.Item style={{margin:"0"}}>
+                    Track car
+                </Menu.Item>
+                <Menu.Item style={{margin:"0"}}>
+                    Parking
+                </Menu.Item>
+                <Menu.Item style={{margin:"0"}}>
+                    Transport goods
+                </Menu.Item>
+
+            </SubMenu>
+            <SubMenu title="Help">
+                <Menu.Item>
+                    Make payment
+                </Menu.Item>
+            </SubMenu>
+            <SubMenu title="Account">
+                <Menu.Item style={{margin:"0"}}>
+                    Login
+                </Menu.Item>
+                <Menu.Item style={{margin:"0"}}>
+                    Register
+                </Menu.Item>
+            </SubMenu>
         </Menu>
     )
 }
