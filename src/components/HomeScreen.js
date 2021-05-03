d79905093d902c5d693872e9610583bb4f987dab
unknown <jorammanoah1@gmail.com>
2021-05-03 14:38:16 -0700

    Black spots

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Row, Card, Button } from "antd";
-import { GlobalOutlined, HourglassFilled} from '@ant-design/icons'
+import { GlobalOutlined, HourglassFilled, WarningFilled } from "@ant-design/icons";
 // import { Link } from "react-router-dom";
 
 export default function HomeScreen() {
-
   return (
-    <div style={{ marginTop: "5rem" }}>
+    <div style={{ marginTop: "3.5rem" }}>
       <Row justify="space-around" align="middle" style={{ marginTop: ".5rem" }}>
         <Card
           style={{
@@ -31,9 +30,9 @@ export default function HomeScreen() {
             borderRadius: "5px",
           }}
         >
-          <h2>About us</h2>
+          <h2 style={{margin:"0"}}>About us</h2>
 
-          <p>
+          <p style={{margin:"0"}}>
             Xpress Cloud is an online based platform that uses ML and AI to
             provide realtime information about traffic flow.
           </p>
@@ -61,16 +60,23 @@ export default function HomeScreen() {
             color: "white",
           }}
         >
-          <p style={{ color: "white", fontSize:"18px", margin:"0" }}> 
-          <div style={{backgroundColor:"#262626", width:"1.5rem", display:"block", margin:"auto", borderRadius:"100px"}}>
-            <HourglassFilled spin />
-          </div>
-           Stuck on traffic ?</p>
-          <p style={{ fontSize: "14px", marginBottom:"0" }}>
-            Find shortest route ...
-            
+          <p style={{ color: "white", fontSize: "18px", margin: "0" }}>
+            <div
+              style={{
+                backgroundColor: "",
+                width: "2.4rem",
+                display: "block",
+                margin: "auto",
+                borderRadius: "150px",
+              }}
+            >
+              <HourglassFilled style={{color:"#262626", fontSize:"2rem"}}  />
+            </div>
+            Stuck on traffic ?
           </p>
-
+          <p style={{ fontSize: "14px", marginBottom: "0" }}>
+            Find shortest route ...
+          </p>
         </Card>
       </Row>
       <Row
@@ -80,22 +86,57 @@ export default function HomeScreen() {
       >
         <Card
           style={{
-            backgroundColor: "rgb(27,85,131)",
+            backgroundColor: "rgb(46,139,87)",
             width: "20rem",
             borderRadius: "5px",
             color: "white",
           }}
         >
-          <p style={{ color: "white", fontSize:"18px",margin:"0" }}>
-          <div style={{backgroundColor:"#262626", width:"1.5rem", display:"block", margin:"auto", borderRadius:"100px"}}>
-            <GlobalOutlined spin />
-          </div>
-             Lost a vehicle?</p>
-          <p style={{ marginBottom:"0"}}>
-            Track your lost vehicle ...
+          <p style={{ color: "white", fontSize: "18px", margin: "0" }}>
+            <div
+              style={{
+                backgroundColor: "",
+                width: "1.5rem",
+                display: "block",
+                margin: "auto",
+                borderRadius: "100px",
+              }}
+            >
+              <GlobalOutlined style={{color:"#262626", fontSize:"2rem"}} />
+            </div>
+            Lost a vehicle?
           </p>
-
-          
+          <p style={{ marginBottom: "0" }}>Track your lost vehicle ...</p>
+        </Card>
+      </Row>
+      <Row
+        justify="space-around"
+        align="middle"
+        style={{ marginTop: ".5rem", textAlign: "center" }}
+      >
+        <Card
+          style={{
+            backgroundColor: "rgb(46,139,87)",
+            width: "20rem",
+            borderRadius: "5px",
+            color: "white",
+          }}
+        >
+          <p style={{ color: "white", fontSize: "18px", margin: "0" }}>
+            <div
+              style={{
+                backgroundColor: "",
+                width: "1.5rem",
+                display: "block",
+                margin: "auto",
+                borderRadius: "100px",
+              }}
+            >
+              <WarningFilled style={{color:"#262626", fontSize:"2rem"}} />
+            </div>
+            Find accidents
+          </p>
+          <p style={{ marginBottom: "0" }}>Identify black spots ...</p>
         </Card>
       </Row>
       {/* <Row justify="space-around" align="middle" style={{ marginTop: ".5rem" }}>
